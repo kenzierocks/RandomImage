23735733b414c0a2cf5e42c1a68cd25b381f8088
Kenzie Togami <ket1999@gmail.com>
2015-07-17 10:52:29 -0700

    Add support for choosing scale

--- src/main/java/me/kenzierocks/WritingRandomImages.java
+++ src/main/java/me/kenzierocks/WritingRandomImages.java
@@ -5,9 +5,9 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.OptionalInt;
 import java.util.Random;
 import java.util.function.Predicate;
 import java.util.regex.Matcher;
@@ -24,12 +24,36 @@ public class WritingRandomImages {
             System.exit(1);
             return;
         }
-        Pattern scalePat = Pattern.compile("-(?:-scale|s)=(\\d+)");
+        Pattern scalePat = Pattern.compile("-(?:-scale|s)(?:-(w|h))?=(\\d+)");
         @SuppressWarnings("unchecked")
         Predicate<String> isVerbose = (Predicate<String>) (Object) Predicate.isEqual("-v").or(Predicate.isEqual("--verbose"));
         boolean verbose = Stream.of(args).anyMatch(isVerbose);
-        int scale = Stream.of(args).map(scalePat::matcher).filter(Matcher::matches).map(matcher -> matcher.group(1))
-                .mapToInt(Integer::parseInt).findFirst().orElse(1);
+        OptionalInt[] packed = Stream.of(args).map(scalePat::matcher).filter(Matcher::matches)
+                .map(matcher -> {
+                    OptionalInt[] ints = new OptionalInt[]{OptionalInt.empty(), OptionalInt.empty()};
+                    OptionalInt scale = OptionalInt.of(Integer.parseInt(matcher.group(2)));
+                    if (matcher.group(1) == null) {
+                        ints[0] = ints[1] = scale;
+                    } else {
+                        if (matcher.group(1).equals("w")) {
+                            ints[0] = scale;
+                        } else {
+                            ints[1] = scale;
+                        }
+                    }
+                    return ints;
+                }).reduce(new OptionalInt[]{OptionalInt.empty(), OptionalInt.empty()}, (id, next) -> {
+                    OptionalInt[] ints = new OptionalInt[2];
+                    OptionalInt aID = id[0];
+                    OptionalInt bID = id[1];
+                    OptionalInt aNx = next[0];
+                    OptionalInt bNx = next[1];
+                    ints[0] = aNx.isPresent() ? aNx : aID;
+                    ints[1] = bNx.isPresent() ? bNx : bID;
+                    return ints;
+                });
+        int scaleWidth = packed[0].orElse(1);
+        int scaleHeight = packed[1].orElse(1);
         args = Stream.of(args).filter(isVerbose.or(scalePat.asPredicate()).negate()).toArray(String[]::new);
         Random r = new Random();
         int width = Integer.parseInt(args[0]);
@@ -49,11 +73,11 @@ public class WritingRandomImages {
                     ((pixels[i + 2] & 0xFF) << 8) |
                     ((pixels[i + 3] & 0xFF))).brighter().brighter().getRGB());
         }
-        int scaledWidth = width * scale;
-        int scaledHeight = height * scale;
+        int scaledWidth = width * scaleWidth;
+        int scaledHeight = height * scaleHeight;
         BufferedImage tmp = new BufferedImage(scaledWidth, scaledHeight, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g = tmp.createGraphics();
-        AffineTransform at = AffineTransform.getScaleInstance(scale, scale);
+        AffineTransform at = AffineTransform.getScaleInstance(scaleWidth, scaleHeight);
         g.drawRenderedImage(created, at);
         created = tmp;
         if (verbose) {
